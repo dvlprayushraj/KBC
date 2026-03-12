3bf29d3091b8e1525ea2acb0655ee43de1b59523
dvlprayushraj <163453771+dvlprayushraj@users.noreply.github.com>
2026-03-13 00:31:22 +0530

    rnadom number guesser

--- new.py
+++ new.py
@@ -1,6 +1,20 @@
-dic = {
-    "name": "John",
-    "age": 30,
-}
+import random
 
-print(dic["name"])
+secret_number = random.randint(1, 10)
+
+guess = None
+
+while guess != secret_number:
+
+    guess = int(input("Guess a number between 1 and 10: "))
+
+    if guess < secret_number:
+        print("Too low! Try again.")
+    elif guess > secret_number:
+        print("Too high! Try again.")
+    else:
+        print("Congratulations! You guessed the number!")
+
+
+
+      
