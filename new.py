1415ce4cb6aeb466fa8eb31305e78997fa622dfd
dvlprayushraj <163453771+dvlprayushraj@users.noreply.github.com>
2026-03-16 14:08:33 +0530

    year finder

--- new.py
+++ new.py
@@ -1,21 +1,34 @@
-a = str((input("Enter a password: ")))
+def find_number_of_month():
 
-def check_password(password):
 
-    if len(password) <8:
-        print("Password must be at least 8 characters long.")
-        return False
-    
-    if not any(char.isupper() for char in password):
-        print("Password must contain at least one uppercase letter.")
-        return False
-    
-    if not any (char in "!@#$%^&*()-+" for char in password):
-        print("Password must contain at least one special character.")
-        return False
-    
-    return "Password is valid."
+    status = int(input("Enter the month number (1-12): "))
 
-   
+    match status:
+        case 1:
+            return "January"
+        case 2:
+            return "February"
+        case 3:
+            return "March"
+        case 4:
+            return "April"
+        case 5:
+            return "May"
+        case 6:
+            return "June"
+        case 7:
+            return "July"
+        case 8:
+            return "August"
+        case 9:
+            return "September"
+        case 10:
+            return "October"
+        case 11:
+            return "November"
+        case 12:
+            return "December"
+        case _:
+            return "Invalid month number"
 
-check_password(a)
+print(find_number_of_month())
