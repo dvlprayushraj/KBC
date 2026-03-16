6ea672ccbfdb26d09f5e6634cfb78ecd516ba546
dvlprayushraj <163453771+dvlprayushraj@users.noreply.github.com>
2026-03-16 13:50:04 +0530

    password checker

--- new.py
+++ new.py
@@ -1,4 +1,21 @@
-f = open('myfile.txt')
+a = str((input("Enter a password: ")))
 
-print(f.read())
-f.close()
+def check_password(password):
+
+    if len(password) <8:
+        print("Password must be at least 8 characters long.")
+        return False
+    
+    if not any(char.isupper() for char in password):
+        print("Password must contain at least one uppercase letter.")
+        return False
+    
+    if not any (char in "!@#$%^&*()-+" for char in password):
+        print("Password must contain at least one special character.")
+        return False
+    
+    return "Password is valid."
+
+   
+
+check_password(a)
