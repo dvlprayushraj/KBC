1a2115cf967b7e52e7b85f817de21bf8b4b95209
dvlprayushraj <163453771+dvlprayushraj@users.noreply.github.com>
2026-03-22 14:56:02 +0530

    checker equl

--- new.py
+++ new.py
@@ -1,22 +1,10 @@
-# Define a function named 'test_prime' that checks if a number 'n' is a prime number
-def test_prime(n):
-    # Check if 'n' is equal to 1
-    if (n == 1):
-        # If 'n' is 1, return False (1 is not a prime number)
-        return False
-    # Check if 'n' is equal to 2
-    elif (n == 2):
-        # If 'n' is 2, return True (2 is a prime number)
-        return True
-    else:
-        # Iterate through numbers from 2 to (n-1) using 'x' as the iterator
-        for x in range(2, n):
-            # Check if 'n' is divisible by 'x' without any remainder
-            if (n % x == 0):
-                # If 'n' is divisible by 'x', return False (not a prime number)
-                return False
-        # If 'n' is not divisible by any number from 2 to (n-1), return True (prime number)
-        return True
+a = [1,2,3,4,5]
+b = [1,2,3,3,5]
 
-# Print the result of checking if 9 is a prime number by calling the 'test_prime' function
-print(test_prime(67))
+
+if (a ==b):
+    print(a,b,"both lis are same \n")
+else:
+    print(a,b,"both list are not same")
+    
+        
