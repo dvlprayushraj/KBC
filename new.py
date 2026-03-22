4a0f373e5250d5b53da0200460bae8e33714f065
dvlprayushraj <163453771+dvlprayushraj@users.noreply.github.com>
2026-03-22 11:08:22 +0530

    prime number checker

--- new.py
+++ new.py
@@ -1,29 +1,22 @@
-def find_the_maximum():
-    a= int(input("enter your first number :"))
-    b= int(input("enter your first number :"))
-    c= int(input("enter your first number :"))
-    
-    
-    if a>b:
-        return a
-    
-    
-    elif a>c:
-        return a
-    
-    elif b>c:
-        
-        return b
-    
-    elif b>a:
-        return b
-    
+# Define a function named 'test_prime' that checks if a number 'n' is a prime number
+def test_prime(n):
+    # Check if 'n' is equal to 1
+    if (n == 1):
+        # If 'n' is 1, return False (1 is not a prime number)
+        return False
+    # Check if 'n' is equal to 2
+    elif (n == 2):
+        # If 'n' is 2, return True (2 is a prime number)
+        return True
     else:
-        return c
+        # Iterate through numbers from 2 to (n-1) using 'x' as the iterator
+        for x in range(2, n):
+            # Check if 'n' is divisible by 'x' without any remainder
+            if (n % x == 0):
+                # If 'n' is divisible by 'x', return False (not a prime number)
+                return False
+        # If 'n' is not divisible by any number from 2 to (n-1), return True (prime number)
+        return True
 
-    
-
-    
-print(find_the_maximum())
-    
-    
+# Print the result of checking if 9 is a prime number by calling the 'test_prime' function
+print(test_prime(67))
