f2586312b8eb533381c0c356db0cd760cd442d37
dvlprayushraj <163453771+dvlprayushraj@users.noreply.github.com>
2026-03-23 10:06:50 +0530

    test_of_the_nums

--- new.py
+++ new.py
@@ -1,16 +1,12 @@
-def verify_integer_or_not(prompt):
-   try:
-       Value = int(input(prompt))
-       
-       return Value
-   
-   except ValueError:
-       print("invalid integer ,inputa a valid integer ")
-       
-       
-       
-n = verify_integer_or_not("input an integer :")
-
-print("input value :",n)
-       
+def test_of_the_nums(nums):
+    try:
+        result = nums.lenght
+        print("the lenght of the list are: ",result)
         
+    except:
+        print("invalid ")
+        
+        
+        
+nums =[1,2,3,4,5,6]
+test_of_the_nums(nums)
