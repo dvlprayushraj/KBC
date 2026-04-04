2caef0c2658f09ce8624c4365ff0fa9b7283d5b7
dvlprayushraj <163453771+dvlprayushraj@users.noreply.github.com>
2026-04-04 12:36:08 +0530

    show the image to the user

--- new.py
+++ new.py
@@ -1,11 +1,27 @@
 import cv2
 
-image = cv2.imread('image.jpg')
+image_location = input("Enter the location of the image: ")
 
-
-if image is  not None:
-    cv2.imshow("image showing ", image)
+if image_location is not None:
+    image = cv2.imread(image_location)
+    
     cv2.waitKey(0)
-    cv2.destroyAllWindows()
-else:
-    print("Image read successfully.")
+    
+grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+cv2.waitKey(0)
+    
+user_choice = input("do you want to save or show the iamge ?// ")
+
+if user_choice == "save":
+    grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite("grey_image.jpg", grey)
+    cv2.imshow("image",image)
+    print("Image saved successfully.")
+
+if user_choice == "show":
+    grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("grey ",grey)
+    cv2.waitKey(0)
+    
+
