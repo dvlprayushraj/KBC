6d47ca4e4afc9ceef4b2329c2a0916fdcb0da34f
dvlprayushraj <163453771+dvlprayushraj@users.noreply.github.com>
2026-04-05 16:36:56 +0530

    flipped

--- new.py
+++ new.py
@@ -1,19 +1,22 @@
 import cv2
 
+
 image = cv2.imread('image.jpg')
 
 if image is None:
-    print("image load sucessfully")
-    
+   print("image could not load")
+   
 else:
-    print("image load failed")
+    (h,w) = image.shape[:2]
     
-    resize_image = cv2.resize(image, (300, 300))
-    cv2.imshow("resized image", resize_image)
+    center = (w//2,h//2)
+    
+    m = cv2.getRotationMatrix2D(center,90,1.0)
+    rotated = cv2.warpAffine(image,m,(h,w))
+    
+    cv2.imshow("original show ", image)
+    cv2.imshow("rotated image ", rotated)
     
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    
-    
-    
     
