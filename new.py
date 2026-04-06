28973ceaa833499aa83b0844b887976b76a152be
dvlprayushraj <163453771+dvlprayushraj@users.noreply.github.com>
2026-04-06 23:15:21 +0530

    show shape acc to user iinput

--- new.py
+++ new.py
@@ -1,22 +1,63 @@
 import cv2
 
+image = cv2.imread(input("enter your image location / formate to display your image : "))
 
-image = cv2.imread('image.jpg')
 
-if image is None:
-   print("image could not load")
+
+if image is not None:
    
-else:
-    (h,w) = image.shape[:2]
-    
-    center = (w//2,h//2)
-    
-    m = cv2.getRotationMatrix2D(center,90,1.0)
-    rotated = cv2.warpAffine(image,m,(h,w))
-    
-    cv2.imshow("original show ", image)
-    cv2.imshow("rotated image ", rotated)
-    
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    
+   print("image loade sucessfully")
+   
+
+
+   cv2.waitKey(0)
+   cv2.destroyAllWindows()
+   
+   
+   
+   
+   user_choice = str(input("what do you wnat to draw on the image \n  1. LINE \n 2. CIRCLE \n 3. RECTANGLE \n 4. TEXT\n\n\n"))
+
+### user want to draw a line
+
+
+
+
+   
+   if user_choice == "line":
+   
+      pt1 =(50,100)
+      pt2 =(300,100)
+      thickness = 5
+      color = (0,0,255)
+   
+      happt = cv2.line(image,pt1,pt2,color,thickness)
+      cv2.imshow("your image", happt)
+      cv2.waitKey(0)
+      cv2.destroyAllWindows()
+   
+   ## if user want to draw an circle 
+   
+   if user_choice == "circle":
+      center =(200,200)
+      radius = 30
+      color = (0,0,255)
+      thickness = 5
+   
+      circle = cv2.circle(image,center,radius,color,thickness)
+      cv2.imshow("here is you circle drawn",circle)
+      cv2.waitKey(0)
+      cv2.destroyAllWindows()
+      
+   if user_choice == "rectangle":
+      pt1 =(50,100)
+      pt2 =(300,10)
+      color = (0,0,255)
+      thickness = 5
+      
+      
+      rectangle = cv2.rectangle(image,pt1,pt2,color,thickness)
+      cv2.imshow("rectangle image ", rectangle)
+      cv2.waitKey(0)
+      cv2.destroyAllWindows()
+      
