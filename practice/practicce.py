4ebdc331a16d75527a13cd720593e7173f9067c4
dvlprayushraj <163453771+dvlprayushraj@users.noreply.github.com>
2026-04-13 11:33:37 +0530

    multiple face detection

---
+++ practice/practicce.py
@@ -0,0 +1,23 @@
+import cv2
+
+image_recoginer = cv2.CascadeClassifier("practice\haarcascade_frontalface_default.xml")
+
+image = cv2.imread('practice\IMG_20250629_172135.jpg')
+
+resize_image = cv2.resize(image,(0,0),fx=0.2, fy=0.2)
+
+
+grey = cv2.cvtColor(resize_image,cv2.COLOR_BGR2GRAY)
+
+faces = image_recoginer.detectMultiScale(grey,1.1,4)
+
+for x,y,w,h in faces:
+    rectange = cv2.rectangle(resize_image,(x,y),(x+w,y+h),(255,0,0),4)
+    
+
+cv2.imshow("image ", resize_image)
+
+cv2.waitKey(0) 
+    
+
+cv2.destroyAllWindows()
